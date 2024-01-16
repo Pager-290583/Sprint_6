656c1da5463833a49bd8ca6162fdf7f5bfc6524a
Oleg <kov2905@icloud.com>
2024-01-16 19:06:44 +0200

    Переписал пользовательские данные даты.  для оформления заказа

--- test_date/user_data.py
+++ test_date/user_data.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 
 class UserData:
@@ -8,6 +9,6 @@ class UserData:
         self.address = f'Москва, Мира 12, д. 12'
         self.station = 'Черкизовская'
         self.telephone = f'+79{random.randint(100, 999999999)}'
-        self.date_zakaz = f'06.02.2024'
+        self.date_zakaz = datetime.date.today().strftime("%d.%m.%Y")
         self.term_data = 'сутки'
         self.comment = 'Предварительно позвонить по номеру телефона'
