4f44cd751f7d13340ee4d46c5fb032d2bc664e38
Oleg <kov2905@icloud.com>
2024-01-16 20:21:43 +0200

    Изменил метод отправки даты: сегодня +1

--- test_date/user_data.py
+++ test_date/user_data.py
@@ -1,6 +1,8 @@
 import random
 import datetime
+from datetime import timedelta, date
 
+end_date = date.today() + timedelta(days=1)
 
 class UserData:
     def __init__(self):
@@ -9,6 +11,6 @@ class UserData:
         self.address = f'Москва, Мира 12, д. 12'
         self.station = 'Черкизовская'
         self.telephone = f'+79{random.randint(100, 999999999)}'
-        self.date_zakaz = datetime.date.today().strftime("%d.%m.%Y")
+        self.date_zakaz = end_date.strftime("%d.%m.%Y")
         self.term_data = 'сутки'
         self.comment = 'Предварительно позвонить по номеру телефона'
